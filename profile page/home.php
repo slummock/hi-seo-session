29c112b830da0aa1d16024a73c6144a5cb67129b
Danny Parkinson <danny.parkinson@laterooms.com>
2015-05-21 13:24:10 +0100

    adding profile page

---
+++ profile page/home.php
@@ -0,0 +1,174 @@
+<?php 
+$home ='<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
+<title>Hyper Island - Digital Apprenticeship</title>
+	</head>
+	
+		<body>
+
+	<section id="header">
+		<div id="main-head">
+			
+			<div class="hyperisland">
+			<i class="icon"><svg class="full not mobile" viewBox="0 0 263 32"><path d="M13.838 12.912v-12.398h6.452v30.972h-6.452v-13.814h-7.389v13.813l-6.449 0v-30.972h6.449v12.398l7.389-0zM31.562 20.761l-8.072-20.247h7.046l4.614 13.084 4.912-13.084h6.578l-8.629 20.247v10.725l-6.449 0v-10.725zM49.672 0.514h11.618c5.895 0 8.201 3.604 8.201 9.266 0 3.518-1.026 9.095-8.586 9.095h-4.783v12.612h-6.449v-30.972zM56.121 14.112h3.589c1.92 0 3.246-2.101 3.246-4.161 0-2.531-0.683-4.675-3.589-4.675h-3.246v8.835zM73.374 31.486v-30.972h16.829v4.763h-10.378v7.636h9.566v4.761h-9.566v9.050h10.721v4.763l-17.172-0zM101.053 31.486h-6.452v-30.972h11.918c4.4 0 7.218 2.832 7.218 8.108 0 3.946-1.538 6.904-5.723 7.593v0.086c1.411 0.17 5.597 0.514 5.597 6.089 0 1.974 0.126 7.808 0.726 9.096h-6.323c-0.854-1.887-0.683-3.99-0.683-6.006 0-3.689 0.343-6.821-4.612-6.821h-1.666l0.001 12.826zM101.053 13.897h2.86c2.563 0 3.289-2.574 3.289-4.546 0-2.961-1.237-4.075-3.289-4.075h-2.86v8.62zM127.959 31.486v-30.972h6.449v30.972l-6.449-0.001zM148.203 32c-7.987 0-9.481-4.718-9.141-10.081h6.452c0 2.958 0.126 5.447 3.672 5.447 2.18 0 3.246-1.415 3.246-3.516 0-5.578-12.984-5.92-12.984-15.142 0-4.848 2.307-8.708 10.167-8.708 6.278 0 9.398 2.832 8.969 9.308h-6.28c0-2.316-0.383-4.677-2.989-4.677-2.094 0-3.332 1.159-3.332 3.305 0 5.877 12.984 5.361 12.984 15.099 0 8.021-5.851 8.965-10.763 8.965zM163.622 31.486v-30.972h6.448v25.825h9.655v5.148l-16.104-0zM180.191 31.486l8.543-30.972h8.501l8.372 30.972h-6.791l-1.709-6.864h-8.797l-1.752 6.864h-6.367zM192.624 6.476h-0.086l-2.949 13.384h6.151l-3.117-13.384zM208.679 31.486v-30.972h8.715l8.201 21.449h0.086v-21.449h6.023v30.972h-8.5l-8.416-22.651h-0.086v22.651l-6.023-0.001zM237.253 0.514h10.764c8.458 0 10.037 5.664 10.037 15.314 0 4.763 0.726 15.658-10.294 15.658h-10.507l0-30.972zM243.702 26.723h3.117c3.931 0 4.7-2.958 4.7-10.723 0-5.705 0-10.723-4.486-10.723h-3.332v21.446zM263.226 2.316c0 1.289-1.145 2.329-2.573 2.329-1.435 0-2.588-1.040-2.588-2.329s1.151-2.316 2.588-2.316c1.429 0 2.573 1.027 2.573 2.316zM262.742 2.316c0-1.040-0.943-1.881-2.089-1.881-1.165 0-2.102 0.841-2.102 1.881s0.936 1.893 2.102 1.893c1.145 0 2.089-0.853 2.089-1.893zM260.326 3.655h-0.678v-2.697h1.255c0.466 0 0.763 0.249 0.763 0.71 0 0.343-0.159 0.598-0.603 0.66v0.012c0.145 0.006 0.59 0.037 0.59 0.523 0 0.174 0.013 0.679 0.075 0.791h-0.665c-0.091-0.162-0.091-0.343-0.091-0.523 0-0.317 0.055-0.591-0.472-0.591h-0.173v1.116l-0.002-0zM260.326 2.16h0.306c0.27 0 0.354-0.224 0.354-0.398 0-0.255-0.138-0.355-0.354-0.355h-0.306v0.754z" fill="#000000"></path></svg><svg class="short only mobile" viewBox="0 0 32 32"><path d="M14.159 13.691v-9.18h4.787v22.933h-4.787v-10.228h-5.484v10.228l-4.787-0v-22.933h4.787v9.18h5.484zM23.101 27.445v-22.933h4.787v22.933h-4.787z" fill="#000000"></path></svg></i>
+				</div>
+			<div class="boom">
+				<h1>Digital Apprenticeship</h1>
+				<h2>Interactive Design &#038; Development</h2>
+				</div>
+			</div>
+		<div id="bordetop"></div>
+	</section>
+	<div id="bodywrap">
+<section id="main-content">
+	<div id="introduction">
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+		Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
+		when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+		It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+		It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div>
+		<h2 id="reality">What\'s this like in reality?</h2>
+	<div id="columns">
+		<div class="left-col">
+			
+			<h3>Designer</h3>
+		<div class="inside-des">
+			</div>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+		Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
+		when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+	</div>
+		
+		<div class="right-col">
+		
+			<h3>Developer</h3>
+			<div class="inside-dev">
+			
+				</div>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+		Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
+		when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+	</div>
+			
+	</div>
+	</section>
+
+
+<section id="jobs">
+	<h2>What Jobs are there?</h2>
+		<div id="table">
+			<table>
+		<tbody>
+	<thead>
+		<tr>
+			<th>Jobs</th>
+			<th>Company</th>
+			<th>Website</th>
+		</tr>
+	</thead>
+		<tr>
+				<td>Front End Developer</td>
+				<td>Code Computer Love</td>
+				<td><a href="http://www.codecomputerlove.com/">Website</a></td>
+		</tr>
+		<tr>
+				<td>UI/UX Designer</td>
+				<td>Igniso</td>
+				<td><a href="https://www.igniso.com/">Website</a></td>
+		</tr>
+		<tr>
+				<td>Website Designer</td>
+				<td>Manchester City Council</td>
+				<td><a href="http://www.manchester.gov.uk/">Website</a></td>
+		</tr>
+		</tbody>
+				</table>
+			</div>
+	</section>
+<section id="important-info">
+<h2>Important Info</h2>
+	<div class="dates">
+	<h3>Dates</h3>
+		<ul id="listdates">
+		
+			<li>Start:<span> September 2015</span></li>
+			<li>End:<span> December 2016</span></li>
+			<li>Length:<span> 63 Weeks</span></li>
+			<li>Apply By:<span> 19th May 2015</span></li>
+			<!--<li>Start:<span></span></li>-->
+			
+			</ul>
+		</div>
+		
+	<div class="require">
+		<h3>Apply</h3>
+			<a class="apply-button" href="#">Apply Now</a>
+		<div class="how-to">
+			<a class="how-to-apply" href="#">How to Apply</a>
+				</div>
+		
+			
+			
+		</div>
+		
+	<div class="headshot">
+	<h3>Requirements</h3>
+		<ul id="yeeha">
+			<li>Greater Manchester based</li>
+			<li>Aged between 18-24</li>
+			<li>English and Maths Grade C or above at G.C.S.E level</li>
+			<li>Either qualifications at Level 3 (A-Levels or vocational qualifications) or relevant work experience</li>
+			<li>Applicants should not already have qualifications at level 4 or above (Bachelors Degree or higher)</li>
+			<li>Evidence of your interest in a relevant area - Developing or Design</li>
+			</ul>
+		</div>
+		</section>
+<section id="practice-skills">
+<h2>Want To Practice Your Skills?</h2>
+	<ul class="useful-pracrtice">
+		<li><a href ="http://www.codecademy.com/"><img src="images/code-academy.svg"></a></li>
+		<li><a href ="https://hackdesign.org/"><img src="images/hack-design.svg"></a></li>
+		<li><a href ="http://codeupmcr.co.uk/"><img src="images/codeup.png"</a></i>
+		<li><a href ="https://github.com/"><img src="images/Github.png"></a></li>
+		</ul>
+</section>
+<section id="thanks">
+	<h2>Thanks to all our Employers</h2>
+	<div class="employers">
+		<ul class="emplist">
+			<li><a href="http://theeword.co.uk/"><img src="images/TEWLOGO.png" alt="TheEword"></a></li>
+			<li><a href="http://thisismn.com/"><img src="images/mn-logo.gif" alt="Magentic North"></a></li>
+			<li id="codecomp"><a href="http://www.codecomputerlove.com/"><img src="images/code-computerlove.png" alt="Code Computer Love"></a></li>
+			<li><a href="http://www.manchester.gov.uk/"><img src="images/MCC.jpg" alt="Manchester City Council"></a></li>
+			<li id="igniso"><a href="https://www.igniso.com/"><img src="images/igniso.png" alt="Igniso"></a></li>
+			<li><a href="http://www.laterooms.com/"><img src="images/LR.gif" alt="Late Rooms"></a></li>
+			
+			</ul>
+			<div id="bottom">
+		<a href="" class="apply">Apply</a>
+		<a href="" class="contact">Contact</a>
+			</div>
+		</div>
+	</section>
+	
+
+
+	
+	
+	
+</div><!--Body Wrap-->
+
+
+		</body>
+
+
+	</html>';
+	
+	
+echo $home;
+?>
